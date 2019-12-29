6251eb720ef00c633b2f91d46ec8e2d50c27389d
Takuya <lukefone.story@gmail.com>
2019-12-30 07:13:46 +0900

    replace words

---
+++ nlp_app/simple_numbers_replacement.py
@@ -0,0 +1,9 @@
+import re
+
+
+def tokenize_numbers(text):
+    return re.sub(r'\d+', ' SOMENUMBER', text)
+
+print(tokenize_numbers('卵を1個買ったよ!'))
+print(tokenize_numbers('卵を2個買ったよ!'))
+print(tokenize_numbers('卵を10個を買ったよ!'))
