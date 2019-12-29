dd0a65b7a0f44787a4a49f8f422ed7415b964a02
Takuya <lukefone.story@gmail.com>
2019-12-30 06:59:49 +0900

    apply stop words

---
+++ nlp_app/part_of_speech_based_stop_words_filtering.py
@@ -0,0 +1,21 @@
+import MeCab
+
+tagger = MeCab.Tagger()
+
+
+def tokenize(text):
+    node = tagger.parseToNode(text)
+    result = []
+    while node:
+        features = node.feature.split(',')
+
+        if features[0] != 'BOS/EOS':
+            if features[0] not in ['助詞', '助動詞']:
+                token = features[6] if features[6] != '*' else node.surface
+                result.append(token)
+        node = node.next
+
+    return result
+
+print(tokenize('本を読んだ。'))
+print(tokenize('本を読みました。'))
